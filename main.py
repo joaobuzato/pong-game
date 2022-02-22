d26328fdaeaa2745070ad0efec11e19de3f68ca6
Buzato <joaobuzato@gmail.com>
2022-02-22 12:10:49 -0300

    created paddle 2 and screen update

--- main.py
+++ main.py
@@ -6,10 +6,19 @@ screen = Screen()
 screen.setup(width=800, height=600)
 screen.bgcolor("black")
 screen.title('Pong Game')
+screen.tracer(0)
 screen.listen()
-paddle = Paddle()
-screen.onkey(key="Up", fun=paddle.move_up)
-screen.onkey(key="Down", fun=paddle.move_down)
+paddle1 = Paddle(player_num=1)
+paddle2 = Paddle(player_num=2)
+
+screen.onkey(key="Up", fun=paddle1.move_up)
+screen.onkey(key="Down", fun=paddle1.move_down)
+screen.onkey(key="w", fun=paddle2.move_up)
+screen.onkey(key="s", fun=paddle2.move_down)
+
+game_is_on = True
+while game_is_on:
+    screen.update()
 
 
 
--- paddle.py
+++ paddle.py
@@ -2,14 +2,17 @@ from turtle import Turtle
 
 class Paddle(Turtle):
 
-    def __init__(self):
+    def __init__(self, player_num=1):
         super().__init__()
         self.penup()
         self.color("white")
         self.shape("square")
         self.shapesize(stretch_len=5)
         self.setheading(90)
-        self.goto(350, 0)
+        if player_num == 1:
+            self.goto(350, 0)
+        if player_num == 2:
+            self.goto(-350, 0)
 
     def move_up(self):
         self.forward(40)
